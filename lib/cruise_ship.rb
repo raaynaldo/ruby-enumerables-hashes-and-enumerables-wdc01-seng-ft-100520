7ed0cbb3f9a7a159117756ec95f588e2d9d45aa0
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-17 01:41:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,8 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each{|key, value|
-    key == "suite_a" && value.start_with("A")
+    if key == "suite_a" && value.start_with("A")
+      return value
+    end
   }
 end
